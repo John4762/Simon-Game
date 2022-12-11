289cb9c5d5cc3596d6bb097241680929a7d5d9f4
John Jais <55910149+John4762@users.noreply.github.com>
2022-12-11 15:33:50 +0530

    Finished adding functionalities

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ var level = 0;
 $(document).keypress(function () {
     if (!started) {
         $("#level-title").text("Level " + level);
+
         nextSequence();
         started = true;
     }
@@ -24,12 +25,42 @@ $(".btn").click(function () {
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.length - 1);
 
 });
+
+function checkAnswer(currentLevel) {
+
+
+    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+
+        if (userClickedPattern.length === gamePattern.length) {
+
+            setTimeout(function () {
+                nextSequence();
+            }, 1000);
+        }
+
+
+    }
+    else {
+
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function () {
+            $("body").removeClass("game-over");
+        }, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+
+    }
+
+}
 
 
 function nextSequence() {
 
+    userClickedPattern = [];
     level++;
     $("#level-title").text("Level " + level);
 
@@ -61,4 +92,9 @@ function animatePress(currentColour) {
 
 }
 
+function startOver() {
+    started = false;
+    level = 0;
+    gamePattern = [];
+}
 
